93509bb134024db7715d35ce787922fb9ae36410
Steven <stevenjcarney@gmail.com>
2020-04-27 20:47:43 -0700

    Adjust for Assignment Guidlines

--- Basic_Javascript_1.js
+++ Basic_Javascript_1.js
@@ -1,8 +1,6 @@
+<!DOCTYPE HTML>
+<html>
 <script>
-	function My_First_Function() {
-		var String = "Kiss me, I'm Irish!";
-		var result = String.fontcolo("green");
-		document.getElementById("Irish").innerHTML = 
-		result;
-		}
+alert("Hello, World.");
 </script>
+</html>
